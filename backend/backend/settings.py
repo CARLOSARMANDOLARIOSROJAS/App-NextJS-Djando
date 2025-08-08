7609682b4cc838cf875fa85de18de51b0117ccee
CARLOSARMANDOLARIOSROJAS <20460440@colima.tecnm.mx>
2025-08-07 22:45:16 -0600

    config de docker

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -130,6 +130,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
      "http://localhost:5173",
      "http://localhost:3000",
+     "http://192.168.1.61:3000",
 ]
 
 # Custom User Model
